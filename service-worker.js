05a43694d590ba82473f7a18cd41562793efc865
TKShadowBlade <katanablack85@gmail.com>
2021-12-11 00:37:30 -0500

    added service worker js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/public/index.html",
+    "/public/index.js",
+    "/public/styles.css",
+    "/public/icons/icon-192x192.png",
+    "/public/icons/icon-512x512.png"
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
