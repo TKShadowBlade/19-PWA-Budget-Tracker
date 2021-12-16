ecf58714a8dca2b9c3d109cb27f3ec02b632328f
TKShadowBlade <katanablack85@gmail.com>
2021-12-15 22:15:48 -0500

    update to svc worker and file locations

---
+++ public/service-worker.js
@@ -0,0 +1,60 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/public/index.html",
+    "/public/index.js",
+    "/public/styles.css",
+    "/public/icons/icon-192x192.png",
+    "/public/icons/icon-512x512.png"
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener('install', (evt) => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Files pre-cached successfully');
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+})
+
