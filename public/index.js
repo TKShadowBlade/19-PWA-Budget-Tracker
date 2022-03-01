ea069880d55b95a8aab961e541c5ed8bf47f0c82
TKShadowBlade <katanablack85@gmail.com>
2022-02-28 21:58:37 -0500

    added code to register service worker

--- public/index.js
+++ public/index.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => {
+        console.log('Registered successfully', reg);
+      });
+  });
+}
+
 let transactions = [];
 let myChart;
 
